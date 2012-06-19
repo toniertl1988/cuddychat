f2a6da783c4a33328323f5ace02f28cb4cd1b753
toniertl1988 <t.ertl@UNISTER2444.(none)>
2012-06-19 14:40:00 +0200

    server files

---
+++ Server/ChatServer.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: t.ertl
+ * Date: 01/19/2012
+ * Time: 14:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+namespace Server
+{
+	/// <summary>
+	/// Description of ChatServer.
+	/// </summary>
+	public class ChatServer : IDisposable
+	{
+		private bool disposed = false;
+		
+		public static Hashtable htUsers = new Hashtable(30);
+		public static Hashtable htConnections = new Hashtable(30);
+		private IPAddress ipAddress;
+		private TcpClient tcpClient;
+		public static event StatusChangedEventHandler StatusChanged;
+		private static StatusChangedEventArgs e;
+		
+		private Thread thrListener;
+		private TcpListener tlsClient;
+		bool ServRunning = false;
+		
+		public ChatServer(IPAddress address)
+		{
+			ipAddress = address;
+		}
+				
+		public static void AddUser(TcpClient tcpUser, string strUsername)
+		{
+			ChatServer.htUsers.Add(strUsername, tcpUser);
+			ChatServer.htConnections.Add(tcpUser, strUsername);
+			SendAdminMessage(htConnections[tcpUser] + " has joined us");
+		}
+		
+		public static void RemoveUser(TcpClient tcpUser)
+		{
+			if (htConnections[tcpUser] != null)
+			{
+				SendAdminMessage(htConnections[tcpUser] + " has left us");
+				ChatServer.htUsers.Remove(ChatServer.htConnections[tcpUser]);
+				ChatServer.htConnections.Remove(tcpUser);
+			}
+		}
+		
+		public static void OnStatusChanged(StatusChangedEventArgs e)
+		{
+			StatusChangedEventHandler statusHandler = StatusChanged;
+			if (statusHandler != null)
+			{
+				statusHandler(null, e);
+			}
+		}
+		
+		public static void SendAdminMessage(string Message)
+		{
+			StreamWriter swSenderSender;
+			e = new StatusChangedEventArgs("Administrator: " + Message);
+			OnStatusChanged(e);
+			TcpClient[] tcpClients = new TcpClient[ChatServer.htUsers.Count];
+			ChatServer.htUsers.Values.CopyTo(tcpClients, 0);
+			for (int i = 0; i < tcpClients.Length; i++)
+			{
+				try
+				{
+					if (Message.Trim() == "" || tcpClients[i] == null)
+					{
+						continue;
+					}
+					swSenderSender = new StreamWriter(tcpClients[i].GetStream());
+					swSenderSender.WriteLine("Administrator: " + Message);
+					swSenderSender.Flush();
+					swSenderSender = null;
+				}
+				catch
+				{
+					RemoveUser(tcpClients[i]);
+				}
+			}
+		}
+		
+		public static void SendMessage(string From, string Message)
+		{
+			StreamWriter swSenderSender;
+			e = new StatusChangedEventArgs(From + " says: " + Message);
+			OnStatusChanged(e);
+			TcpClient[] tcpClients = new TcpClient[ChatServer.htUsers.Count];
+			ChatServer.htUsers.Values.CopyTo(tcpClients, 0);
+			for (int i = 0; i < tcpClients.Length; i++)
+			{
+				try
+				{
+					if (Message.Trim() == "" || tcpClients[i] == null)
+					{
+						continue;
+					}
+					else
+					{
+						swSenderSender = new StreamWriter(tcpClients[i].GetStream());
+		                swSenderSender.WriteLine(From + " says: " + Message);
+		                swSenderSender.Flush();
+		                swSenderSender = null;
+					}
+				}
+				catch
+				{
+					RemoveUser(tcpClients[i]);
+				}
+			}
+		}
+		
+		public void StartListening()
+		{
+			IPAddress ipaLocal = ipAddress;
+			tlsClient = new TcpListener(ipaLocal, 8118);
+			tlsClient.Start();
+			ServRunning = true;
+			thrListener = new Thread(KeepListening);
+            thrListener.Start();
+		}
+		
+		private void KeepListening()
+		{
+			while (ServRunning == true)
+			{
+				tcpClient = tlsClient.AcceptTcpClient();
+				Connection newConnection = new Connection(tcpClient);
+			}
+		}
+		
+	    public void Dispose()
+    	{
+		    Dispose(true);
+		    GC.SuppressFinalize(this);
+    	}
+	    
+        protected virtual void Dispose(bool disposing)
+    	{
+	        if (!disposed)
+	        {
+	            if (disposing)
+	            {
+	                // Free other state (managed objects).
+	            }
+	            // Free your own state (unmanaged objects).
+	            // Set large fields to null.
+	            StopListening();
+	            disposed = true;
+	        }
+    	}
+        
+        public void StopListening()
+        {
+        	ServRunning = false;
+        	thrListener.Abort();
+        	tlsClient.Stop();
+        }
+        
+        ~ChatServer()
+        {
+        	Dispose (false);
+        }
+	}
+}
---
+++ Server/Window1.xaml.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: t.ertl
+ * Date: 01/19/2012
+ * Time: 14:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	///	
+	
+	public partial class Window1 : Window
+	{
+		private delegate void UpdateStatusCallback(string strMessage);
+
+        private ChatServer mainServer;
+		
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		
+		private void btnListen_Click(object sender, EventArgs e)
+		{
+			IPAddress ipAddr = IPAddress.Parse(txtIp.Text);
+			mainServer = new ChatServer(ipAddr);
+			ChatServer.StatusChanged += new StatusChangedEventHandler(mainServer_StatusChanged);
+			mainServer.StartListening();
+			txtLog.AppendText("Monitoring for connections...\r");
+		}
+		
+		public void mainServer_StatusChanged(object sender, StatusChangedEventArgs e)
+		{
+    		// Call the method that updates the form
+    		this.Dispatcher.Invoke(new UpdateStatusCallback(this.UpdateStatus), new object[] { e.EventMessage });
+		}
+		
+		private void UpdateStatus(string strMessage)
+		{
+			// Updates the log with the message
+			txtLog.AppendText(strMessage + "\r");
+			txtLog.ScrollToEnd();
+		}
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        	if (mainServer != null) {
+        		mainServer.StopListening();
+        	}
+        }
+	}
+}
