fa2355806faafcd9e49843bb6390a7dbad218a07
Toni Ertl <toni.ertl@unister.de>
2015-05-20 09:54:55 +0200

    updated Readme

---
+++ Client/PrivateWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: t.ertl
+ * Date: 14.02.2014
+ * Time: 14:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Client
+{
+	/// <summary>
+	/// Interaction logic for PrivateWindow.xaml
+	/// </summary>
+	public partial class PrivateWindow : Page
+	{
+		public PrivateWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
